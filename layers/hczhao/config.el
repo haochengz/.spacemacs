77822e9488630b87d23401ead968cb179feee86b
haochengz <haochengzhao@outlook.com>
2018-05-05 12:33:09 +0800

    add new file config.el and keybindings.el

---
+++ layers/hczhao/config.el
@@ -0,0 +1,5 @@
+;; --Set escape key-binding to "jj"
+(setq-default evil-escape-key-sequence "jj")
+(setq-default evil-escape-delay 0.2)
+
+(setq global-linum-mode t)
---
+++ layers/hczhao/keybingdings.el
@@ -0,0 +1,3 @@
+ 
+;; --Set escape key-binding to "jj"
+(setq-default evil-escape-key-sequence "jj")
